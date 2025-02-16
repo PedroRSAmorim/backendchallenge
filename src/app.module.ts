3c85dd49d889481faababe19c2ef64fe0679617a
Pedro_AmorimSeal <pedro.amorim@convergint.com>
2025-02-16 15:54:17 -0300

    Security - Removing credentials from source code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import * as dotenv from 'dotenv'
 import { HttpStatus, Module } from '@nestjs/common'
 import { GraphQLError } from 'graphql'
 import { TypeOrmModule } from '@nestjs/typeorm'
@@ -8,15 +9,17 @@ import { CompanyModule } from 'src/company'
 import { ApolloDriver } from '@nestjs/apollo'
 import { ConfigModule } from '@nestjs/config'
 
+dotenv.config()
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'challenge',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: false,
     }),
--- src/database/data-source.database.ts
+++ src/database/data-source.database.ts
@@ -1,12 +1,15 @@
+import * as dotenv from 'dotenv'
 import { DataSource } from 'typeorm'
+
+dotenv.config()
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'challenge',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migration/*{.ts,.js}'],
 })
